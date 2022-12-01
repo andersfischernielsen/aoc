d7e92e55f1cdadf8060e292d2b97ee9db6753878
Anders Fischer-Nielsen <andersfischern@me.com>
2022-12-01 10:56:06 +0100

    Create project

---
+++ aoc-2022/target/debug/aoc-2022.d
@@ -0,0 +1,1 @@
+/Users/fischer/Documents/pleo-io/aoc/aoc-2022/target/debug/aoc-2022: /Users/fischer/Documents/pleo-io/aoc/aoc-2022/src/main.rs
---
+++ aoc-2022/target/debug/deps/aoc_2022-2328fcacd2deacca.d
@@ -0,0 +1,5 @@
+/Users/fischer/Documents/pleo-io/aoc/aoc-2022/target/debug/deps/aoc_2022-2328fcacd2deacca.rmeta: src/main.rs
+
+/Users/fischer/Documents/pleo-io/aoc/aoc-2022/target/debug/deps/aoc_2022-2328fcacd2deacca.d: src/main.rs
+
+src/main.rs:
